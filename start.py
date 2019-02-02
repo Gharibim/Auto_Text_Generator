2f1cb646699e7c7fe993a15b03646477f8a3f022
Mohamed Gharibi <gharibimo@gmail.com>
2019-02-01 20:23:04 -0600

    Update start.py

--- start.py
+++ start.py
@@ -2,11 +2,11 @@ from textgenrnn import textgenrnn
 
 textgen = textgenrnn()
 
-textgen.train_from_file('LSTM_In.txt', num_epochs=2)
+textgen.train_from_file('In.txt', num_epochs=2)
 
 textgen.generate()
 
-writer = open("file10k.txt", 'w+')
+writer = open("file10k.txt", 'w+')     # to generate 10k captions
 
 for i in range(10000):
     writer.write(str(textgen.generate(1, True)[0]))
